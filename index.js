ba435738fc52d4688c1c0af4daf825c644d4abca
kcheykim <kosalcheykim@gmail.com>
2021-11-10 18:04:07 -0800

    able to write title to create file called README

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-//const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [{
         type: 'input',
-        name: 'proj-title',
+        name: 'title',
         message: 'What is the title of your project?'
     },
     {
@@ -48,11 +49,17 @@ const questions = [{
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) { console.log("There was an error", err) }
+        console.log("Successfully writes out to file.");
+    });
+}
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions);
+    inquirer.prompt(questions)
+        .then(data => writeToFile('README', generateMarkdown(data)));
 }
 
 // Function call to initialize app
